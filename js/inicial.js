0d165ce5cfacde0ed8d03553e3886352811f26b6
jarad hildebrando <jarad_hildebrando@hotmail.com>
2020-11-16 17:51:40 -0300

    ajustes-responsivo

--- js/inicial.js
+++ js/inicial.js
@@ -8,30 +8,56 @@ function iniciarJogo() {
 }
 
 function infosJogo() {
+    var post = document.getElementById('container')
     var dados = document.createElement('div')
         dados.className = 'row'
         dados.innerHTML = '<div class="col"><div id="caixa" ><div id="infos"></div></div></div>'
     var nivel = document.getElementById('nivel').value
     if (nivel == '') {
 
-        document.body.appendChild(dados)
-        document.getElementById('infos').remove()
+        if (document.getElementById('infos')) {
+            post.lastElementChild.remove()
+        }
+        
+    }if (nivel == 'facil'){
+
+        if (document.getElementById('infos')) {
+            post.lastElementChild.remove()
+        }
+        
+
+        post.appendChild(dados)
+        
+        document.getElementById('infos').innerHTML = '<img class="mosquito1" src="./img/mosca.png" >: O Mosquito azul tem de ser clicado ate o tempo de 2.5s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo<br><img class="mosquito1" src="./img/mosca1.png" >: O Mosquito vermelho não pode ser clicado ate 2.5s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo<br><img class="mosquito1" src="./img/mosca2.png" >: O Mosquito verde deve ser clicado ate 2.5s, onde ele some da tela, para ganhar um <img src="./img/coracao_cheio.png"> no lugar de um que tenha sido perdido<br>'
+        console.log(dados)   
     }
     if (nivel == 'normal'){
 
-        document.body.appendChild(dados)
-        document.getElementById('infos').style.color = '#fff'
-        document.getElementById('infos').style.background = '#000'
+        if (document.getElementById('infos')) {
+            post.lastElementChild.remove()
+        }
+        
+
+        post.appendChild(dados)
         
         document.getElementById('infos').innerHTML = '<img class="mosquito1" src="./img/mosca.png" >: O Mosquito azul tem de ser clicado ate o tempo de 1.5s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo <br><img class="mosquito1" src="./img/mosca1.png" >: O Mosquito vermelho não pode ser clicado ate 1.5s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo <br><img class="mosquito1" src="./img/mosca2.png" >: O Mosquito verde deve ser clicado ate 1.5s, onde ele some da tela, para ganhar um <img src="./img/coracao_cheio.png"> no lugar de um que tenha sido perdido <br>'
         console.log(dados)   
     }
     if (nivel == 'dificil') {
-        document.body.appendChild(dados)
+
+        if (document.getElementById('infos')) {
+            post.lastElementChild.remove()
+        }
+        
+        post.appendChild(dados)
         document.getElementById('infos').innerHTML = '<img class="mosquito1" src="./img/mosca.png" >: O Mosquito azul tem de ser clicado ate o tempo de 1s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo <br><img class="mosquito1" src="./img/mosca1.png" >: O Mosquito vermelho não pode ser clicado ate 1s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo <br><img class="mosquito1" src="./img/mosca2.png" >: O Mosquito verde deve ser clicado ate 1s, onde ele some da tela, para ganhar um <img src="./img/coracao_cheio.png"> no lugar de um que tenha sido perdido <br>'
     }
     if (nivel == 'chucknorris') {
-        document.body.appendChild(dados)
+        if (document.getElementById('infos')) {
+            post.lastElementChild.remove()
+        }
+        
+        post.appendChild(dados)
         document.getElementById('infos').innerHTML = '<img class="mosquito1" src="./img/mosca.png" >: O Mosquito azul tem de ser clicado ate o tempo de 0.75s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo<br><img class="mosquito1" src="./img/mosca1.png" >: O Mosquito vermelho não pode ser clicado ate 0.75s, onde ele some da tela, para não perder um <img src="./img/coracao_cheio.png"> pois ao zerar perder o jogo<br><img class="mosquito1" src="./img/mosca2.png" >: O Mosquito verde deve ser clicado ate 0.75s, onde ele some da tela, para ganhar um <img src="./img/coracao_cheio.png"> no lugar de um que tenha sido perdido<br>'
     }
 
